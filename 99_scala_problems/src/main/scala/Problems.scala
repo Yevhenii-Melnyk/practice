902ae1a9d272751920f553e4eee8a777edb2fd64
Yevhenii Melnyk <yevhenii_melnyk@epam.com>
2016-07-01 21:50:44 +0300

    Problem2

--- 99_scala_problems/src/main/scala/Problems.scala
+++ 99_scala_problems/src/main/scala/Problems.scala
@@ -12,3 +12,24 @@ object Problem1 extends App {
   assert(last1(List(1, 1, 2, 3, 5, 8)) == 8)
 
 }
+
+object Problem2 extends App {
+
+  def lastBut1[T](list: List[T]): T = list.init.last
+
+
+  def lastBut1Rec[T](list: List[T]): T = list match {
+    case x :: _ :: Nil => x
+    case _ :: tail => lastBut1Rec(tail)
+  }
+
+  def lastNthBuiltin[A](n: Int, ls: List[A]): A = {
+    ls.takeRight(n).head
+  }
+
+  println(lastNthBuiltin(4, List(1, 1, 2, 3, 5, 8)))
+
+  assert(lastBut1(List(1, 1, 2, 3, 5, 8)) == 5)
+  assert(lastBut1Rec(List(1, 1, 2, 3, 5, 8)) == 5)
+
+}

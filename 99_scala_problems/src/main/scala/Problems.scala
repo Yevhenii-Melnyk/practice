d646fb05561d04d306ef47879f8b97398573d19b
Yevhenii Melnyk <yevhenii_melnyk@epam.com>
2016-07-03 15:49:41 +0300

    Problem15

--- 99_scala_problems/src/main/scala/Problems.scala
+++ 99_scala_problems/src/main/scala/Problems.scala
@@ -264,3 +264,11 @@ object Problem14 extends App {
     == List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd))
 
 }
+
+object Problem15 extends App {
+
+  def duplicateN[A](n: Int, list: List[A]) = list flatMap (List.fill(n)(_))
+
+  assert(duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+    == List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd))
+}

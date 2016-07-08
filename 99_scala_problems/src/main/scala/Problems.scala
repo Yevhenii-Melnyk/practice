77e2fc9a17a9cdd5b82a569cce081e90aa823b16
Yevhenii Melnyk <yevhenii_melnyk@epam.com>
2016-07-08 13:22:20 +0300

    Problem26. Combinations. copy-pasted

--- 99_scala_problems/src/main/scala/Problems.scala
+++ 99_scala_problems/src/main/scala/Problems.scala
@@ -409,3 +409,24 @@ object Problem25 extends App {
   println(permutation(random.nextInt(permutation.length)))
 
 }
+
+
+object Problem26 extends App {
+
+  def flatMapSublists[A, B](ls: List[A])(f: (List[A]) => List[B]): List[B] =
+    ls match {
+      case Nil => Nil
+      case sublist@(_ :: tail) => f(sublist) ::: flatMapSublists(tail)(f)
+    }
+
+  def combinations[A](n: Int, ls: List[A]): List[List[A]] =
+    if (n == 0) List(Nil)
+    else flatMapSublists(ls) { sl =>
+      combinations(n - 1, sl.tail) map {
+        sl.head :: _
+      }
+    }
+
+
+  println(combinations(3, List(1, 2, 3, 4, 5)))
+}

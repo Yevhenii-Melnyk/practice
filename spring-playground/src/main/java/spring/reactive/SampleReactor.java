33eba45342c5e1f00e5b27c7705374775ed3dfb5
Yevhenii Melnyk <melnyk.yevhenii@gmail.com>
2016-11-29 17:48:23 +0200

    Reactor

--- spring-playground/src/main/java/spring/reactive/SampleReactor.java
+++ spring-playground/src/main/java/spring/reactive/SampleReactor.java
@@ -1,32 +1,17 @@
 package spring.reactive;
 
-import reactor.core.Cancellation;
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.Arrays;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-import static org.springframework.boot.Banner.Mode.LOG;
 
 public class SampleReactor {
 
-	public static void main(String[] args) throws InterruptedException {
-//		EmitterProcessor<String> stream = EmitterProcessor.<String>create().connect();
-//		Flux<String> stringFlux = stream.map(String::toUpperCase)
-//				.doOnNext(s -> System.out.println("1  " + s));
-//		stringFlux.doOnNext(s -> System.out.println("2  " + s));
-//		stringFlux.subscribe();
-//		stream.onNext("Hello World!");
-
-		Flux<Integer> repeat = Flux.fromIterable(Arrays.asList(1, 2, 3)).repeat(3);
-
-		Mono<Integer> next = repeat.next().doOnNext(n -> System.out.println(n));
-
-		next.block();
-
-	}
+    public static void main(String[] args) {
+        EmitterProcessor<String> stream = EmitterProcessor.<String>create().connect();
+        Flux<String> flux = stream
+                .doOnNext(s -> System.out.println("1 " + s));
+        flux .doOnNext(s -> System.out.println("2 " + s));
+        flux.subscribe();
+        stream.onNext("Hello");
+    }
 
 }

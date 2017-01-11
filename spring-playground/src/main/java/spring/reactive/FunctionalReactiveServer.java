e2a7f1760f343e2f4b47b44574efc962e8208e7d
Yevhenii Melnyk <yevhenii.melnyk2@travisperkins.co.uk>
2017-01-11 11:30:11 +0200

    Update functional reactive server sample

--- spring-playground/src/main/java/spring/reactive/FunctionalReactiveServer.java
+++ spring-playground/src/main/java/spring/reactive/FunctionalReactiveServer.java
@@ -1,19 +1,18 @@
 package spring.reactive;
 
-import org.reactivestreams.Publisher;
 import org.springframework.http.server.reactive.HttpHandler;
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
-import org.springframework.web.reactive.function.RouterFunction;
-import org.springframework.web.reactive.function.ServerRequest;
-import org.springframework.web.reactive.function.ServerResponse;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import reactor.ipc.netty.http.server.HttpServer;
 
 import java.io.IOException;
 
-import static org.springframework.web.reactive.function.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.RouterFunctions.route;
-import static org.springframework.web.reactive.function.RouterFunctions.toHttpHandler;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RouterFunctions.toHttpHandler;
 
 public class FunctionalReactiveServer {
 
@@ -21,7 +20,7 @@ public class FunctionalReactiveServer {
 	public static final int PORT = 8080;
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		RouterFunction<?> route = route(GET("/sayHello"), FunctionalReactiveServer::sayHelloHandler);
+		RouterFunction<ServerResponse> route = route(GET("/hello"), FunctionalReactiveServer::sayHelloHandler);
 		HttpHandler httpHandler = toHttpHandler(route);
 
 		ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
@@ -32,7 +31,7 @@ public class FunctionalReactiveServer {
 		System.in.read();
 	}
 
-	public static ServerResponse<Publisher<String>> sayHelloHandler(ServerRequest request) {
+	public static Mono<ServerResponse> sayHelloHandler(ServerRequest request) {
 		return ServerResponse.ok().body(Mono.just("Hello!"), String.class);
 	}
 

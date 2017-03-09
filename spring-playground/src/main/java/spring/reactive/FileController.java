b7cdb06dd5935371204ad9ebbdf9f31bf7eb5a10
Yevhenii Melnyk <yevhenii.melnyk2@travisperkins.co.uk>
2017-03-09 14:46:15 +0200

    Play chunked video

--- spring-playground/src/main/java/spring/reactive/FileController.java
+++ spring-playground/src/main/java/spring/reactive/FileController.java
@@ -2,8 +2,7 @@ package spring.reactive;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ZeroCopyHttpOutputMessage;
+import org.springframework.http.*;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
@@ -19,13 +18,26 @@ public class FileController {
 	@GetMapping("/file")
 	@ResponseBody
 	public Mono<Void> getFile(ServerHttpRequest request, ServerHttpResponse response) {
+//		System.out.println(request.getHeaders());
+		HttpRange range = request.getHeaders().getRange().get(0);
 		try {
 			ZeroCopyHttpOutputMessage zeroCopyResponse = (ZeroCopyHttpOutputMessage) response;
 			Resource logo = new ClassPathResource("fish.mp4");
 			File file = logo.getFile();
-			zeroCopyResponse.getHeaders().setContentType(new MediaType("video", "mp4"));
-			zeroCopyResponse.getHeaders().setContentLength(file.length());
-			return zeroCopyResponse.writeWith(file, 0, file.length());
+			long length = file.length();
+			long chunk = length / 4;
+			HttpHeaders headers = zeroCopyResponse.getHeaders();
+			headers.set("Accept-Ranges", "bytes");
+			headers.setContentType(new MediaType("video", "mp4"));
+			long rangeStart = range.getRangeStart(length);
+			long rangeEnd = Math.min(rangeStart + chunk, length - 1);
+			headers.setContentLength(chunk);
+			if (rangeEnd < length)
+				response.setStatusCode(HttpStatus.PARTIAL_CONTENT);
+			headers.set("Content-Range", String.format("bytes %d-%d/%d", rangeStart, rangeEnd, length));
+			headers.setCacheControl("no-cache");
+			headers.setContentDisposition(ContentDisposition.parse("inline"));
+			return zeroCopyResponse.writeWith(file, rangeStart, chunk);
 		} catch (Throwable ex) {
 			return Mono.error(ex);
 		}
